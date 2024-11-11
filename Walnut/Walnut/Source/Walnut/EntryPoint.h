40a9c82392bad6adf4f541dc9be2730bec84b8a5
huseyinnates <alternativerealitystudiomain@gmail.com>
2024-11-11 04:01:23 +0300

    engine future

--- Walnut/Walnut/Source/Walnut/EntryPoint.h
+++ Walnut/Walnut/Source/Walnut/EntryPoint.h
@@ -1,20 +1,42 @@
+// Main.cpp
 #pragma once
+
+#include "Walnut/Application.h"
+#include "C:/Company/C++/UI/Walnut/Walnut/Source/Walnut/Engine/first_app.hpp"
+
+// std
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
 
 extern Walnut::Application* Walnut::CreateApplication(int argc, char** argv);
 bool g_ApplicationRunning = true;
 
 namespace Walnut {
 
-	int Main(int argc, char** argv)
-	{
-		while (g_ApplicationRunning)
-		{
+	int Main(int argc, char** argv) {
+
+
+		lve::FirstApp app{};
+
+		try {
+			app.run();
+		}
+		catch (const std::exception& e) {
+			std::cerr << e.what() << '\n';
+			return EXIT_FAILURE;
+		}
+
+		return EXIT_SUCCESS;
+		/*
+		while (g_ApplicationRunning) {
 			Walnut::Application* app = Walnut::CreateApplication(argc, argv);
 			app->Run();
 			delete app;
 		}
-
 		return 0;
+		
+		*/
 	}
 
 }
@@ -23,15 +45,13 @@ namespace Walnut {
 
 #include <Windows.h>
 
-int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow)
-{
+int APIENTRY WinMain(HINSTANCE hInst, HINSTANCE hInstPrev, PSTR cmdline, int cmdshow) {
 	return Walnut::Main(__argc, __argv);
 }
 
 #else
 
-int main(int argc, char** argv)
-{
+int main(int argc, char** argv) {
 	return Walnut::Main(argc, argv);
 }
 
